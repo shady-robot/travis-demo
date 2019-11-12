44a45eb8b54a63675f3aa1e5bd91676968669d52
shady-robot <knightdawner@gmail.com>
2019-11-12 14:33:24 +0800

    feat: test precommit configure

--- neon/apps.py
+++ neon/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class NeonConfig(AppConfig):
-    name = 'neon'
+    name = "neon"
---
+++ neon/tests/test_success.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class SuccessTests(TestCase):
+    def test_mean_to_success(self):
+        self.assertEqual(1, 1)
